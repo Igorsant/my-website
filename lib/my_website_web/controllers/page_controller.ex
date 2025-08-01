3301588cc5e2086b36244c215a14cd3ab92797fa
Igor Oliveira dos Santos <igorsantos381@gmail.com>
2025-08-01 18:12:32 -0300

    created home page, missing footer

--- lib/my_website_web/controllers/page_controller.ex
+++ lib/my_website_web/controllers/page_controller.ex
@@ -3,8 +3,11 @@ defmodule MyWebsiteWeb.PageController do
 
   def home(conn, _params) do
     navItems = [%{active: true, label: "Home"}, %{active: false, label: "About"}, %{active: false, label: "Contact"}]
-    [first | _rest] = navItems
-    IO.inspect(first.active)
-    render(conn, :home, navItems: navItems)
+    projectCards = [
+      %{ title: "project 1", description: "description", href: "href"},
+      %{ title: "project 2", description: "description 2", href: "href2"}
+    ]
+
+    render(conn, :home, navItems: navItems, projectCards: projectCards)
   end
 end
--- lib/my_website_web/controllers/page_html.ex
+++ lib/my_website_web/controllers/page_html.ex
@@ -7,4 +7,45 @@ defmodule MyWebsiteWeb.PageHTML do
   use MyWebsiteWeb, :html
 
   embed_templates "page_html/*"
+
+  slot :inner_block, required: true
+
+  def h2(assigns) do
+    ~H"""
+      <h2 class="text-3xl font-semibold">
+        {render_slot(@inner_block)}
+      </h2>
+    """
+  end
+
+  slot :inner_block, required: true
+
+  def p(assigns) do
+    ~H"""
+      <h3 class="mt-5 text-gray-400">
+        {render_slot(@inner_block)}
+      </h3>
+    """
+  end
+
+  attr :title, :string, required: true
+  attr :description, :string, required: true
+  attr :href, :string, required: true
+  def projects_card(assigns) do
+    ~H"""
+    <article class="relative isolate flex flex-col overflow-hidden rounded-2xl bg-gray-900 px-8 pb-5 pt-5">
+      <h3 class="text-lg/6 font-semibold text-white">
+        {@title}
+      </h3>
+      <.p>
+        {@description}
+      </.p>
+      <div class="mt-auto pt-5">
+        <a href={@href} target="blank">
+          View on github
+        </a>
+      </div>
+    </article>
+    """
+  end
 end
