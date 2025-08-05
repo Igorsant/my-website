fd2c74470e9b5ce8e36b30e14acb20b00420c5f1
Igor Oliveira dos Santos <igorsantos381@gmail.com>
2025-08-05 10:24:19 -0300

    finish home page, introducing about page

--- lib/my_website_web/controllers/page_controller.ex
+++ lib/my_website_web/controllers/page_controller.ex
@@ -2,12 +2,18 @@ defmodule MyWebsiteWeb.PageController do
   use MyWebsiteWeb, :controller
 
   def home(conn, _params) do
-    navItems = [%{active: true, label: "Home"}, %{active: false, label: "About"}, %{active: false, label: "Contact"}]
+    navItems = [%{active: true, label: "Home", path: "/"}, %{active: false, label: "About", path: "about"}, %{active: false, label: "Contact"}]
     projectCards = [
       %{ title: "project 1", description: "description", href: "href"},
       %{ title: "project 2", description: "description 2", href: "href2"}
     ]
 
     render(conn, :home, navItems: navItems, projectCards: projectCards)
+  end
+
+  def about(conn, _params) do
+    navItems = [%{active: false, label: "Home", path: "/"}, %{active: true, label: "About", path: "about"}, %{active: false, label: "Contact"}]
+
+    render(conn, :about, navItems: navItems)
   end
 end
